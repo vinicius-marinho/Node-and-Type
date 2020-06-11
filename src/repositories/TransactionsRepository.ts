d358a775b14465f74e99ce874d833c31b637c19c
vinicius.marinho <vinicius.marinho@g.globo>
2020-06-11 01:33:59 -0300

    Finishing the challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
 import Transaction from '../models/Transaction';
 
+
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,33 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+
+    let income = 0;
+    let outcome = 0;
+    let total = 0;
+
+    this.transactions.map(value => {
+      if(value.type === 'income'){
+        income += value.value
+      }else {
+        outcome += value.value
+      }
+    });
+
+    total = income - outcome;
+
+    return {income, outcome, total}
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title, value, type}: TransactionDTO): Transaction {
+    const transaction = new Transaction({title, value, type});
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
